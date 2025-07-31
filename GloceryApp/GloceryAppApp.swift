57eadd3290047f75fd13aa6daf2f401292c6232b
swapnil bhosale <bhosalesb07@gmail.com>
2025-07-31 17:46:41 +0530

    Initial Commit

---
+++ GloceryApp/GloceryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GloceryAppApp.swift
+//  GloceryApp
+//
+//  Created by Apple on 31/07/25.
+//
+
+import SwiftUI
+
+@main
+struct GloceryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
